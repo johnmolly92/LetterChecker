baf743184780708fa173ad90fba7358a88736e16
John Molloy <john.molloy9@mail.dcu.com>
2014-03-05 11:22:10 +0000

    Teacher list

---
+++ LetterChecker/src/com/example/letterchecker/ListOfTeachers.java
@@ -0,0 +1,22 @@
+package com.example.letterchecker;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ListOfTeachers extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_list_of_teachers);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.list_of_teachers, menu);
+		return true;
+	}
+
+}
